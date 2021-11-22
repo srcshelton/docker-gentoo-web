f3964e05f3dde052dedb26e2b0547c231450f8e9
Stuart Shelton <stuart@shelton.me>
2021-11-22 00:49:02 +0000

    Sync common tools
    Signed-off-by: Stuart Shelton <stuart@shelton.me>

--- common/vars.sh
+++ common/vars.sh
@@ -1,11 +1,15 @@
 #! /bin/sh
 #shellcheck disable=SC2034
 
-# Since we're now using 'podman system info' to determine the graphRoot directory, we
-# need to be root simply to setup the environment appropriately :(
-if [ $(( $( id -u ) )) -ne 0 ]; then
-	echo >&2 "FATAL: Please re-run '$( basename "${0}" )' as user 'root'"
-	exit 1
+# Since we're now using 'podman system info' to determine the graphRoot
+# directory, we need to be root simply to setup the environment
+# appropriately :(
+# Support 'core' user for podman+machine Fedora default user...
+if [ "$( uname -s )" != 'Darwin' ]; then
+	if [ $(( $( id -u ) )) -ne 0 ] && [ "$( id -un )" != 'core' ]; then
+		echo >&2 "FATAL: Please re-run '$( basename "${0}" )' as user 'root'"
+		exit 1
+	fi
 fi
 
 # Alerting options...
@@ -36,14 +40,25 @@ fi
 #
 environment_filter='^(declare -x|export) (COLUMNS|EDITOR|GENTOO_PROFILE|HOME|HOSTNAME|LESS(OPEN)?|LINES|LS_COLORS|(MAN)?PAGER|(OLD)?PWD|PATH|(|SYS|PORTAGE_CONFIG)ROOT|SHLVL|TERM)='
 
+use_cpu_arch="$( uname -m | cut -c 1-3 | sed 's/aar/arm/' )"
 if command -v cpuid2cpuflags >/dev/null 2>&1; then
-	#cpuid2cpuflags | cut -d':' -f 2- | sed 's/ / cpu_flags_x86_/g'
-	#use_cpu_flags="$( cpuid2cpuflags | cut -d':' -f 2- | sed 's/ / cpu_flags_x86_/g' )"
-	use_cpu_arch="$( uname -m | cut -c 1-3 | sed 's/aar/arm/' )"
 	use_cpu_flags="$( cpuid2cpuflags | cut -d':' -f 2- )"
 else
-	description="$( grep -E '(model name|Raspberry)' /proc/cpuinfo | sort | tail -n 1 )"
-	case "${description}" in
+	if [ "$( uname -s )" = 'Darwin' ]; then
+		description="$( sysctl -n machdep.cpu.brand_string )"
+	else
+		description="$(
+			grep -E '(model name|Raspberry)' /proc/cpuinfo |
+			sort |
+			tail -n 1
+		)" || :
+	fi
+	if [ -z "${description:-}" ]; then
+		# TODO: Is this a good UID to use (without further checks)?
+		description="$( grep -F 'CPU part' /proc/cpuinfo | sort | tail -n 1 )"
+	fi
+
+	case "${description:-}" in
 		*': Intel(R) Atom(TM) CPU '*' 330 '*' @ '*)
 			use_cpu_arch='x86'
 			use_cpu_flags="mmx mmxext sse sse2 sse3 ssse3" ;;
@@ -53,6 +68,9 @@ else
 		*': Intel(R) Core(TM) i5-24'*' CPU @ '*)
 			use_cpu_arch='x86'
 			use_cpu_flags="aes avx mmx mmxext pclmul popcnt sse sse2 sse3 sse4_1 sse4_2 ssse3" ;;
+		*': Intel(R) Xeon(R) CPU E5-'*' v2 @ '*)
+			use_cpu_arch='x86'
+			use_cpu_flags="aes avx f16c mmx mmxext pclmul popcnt rdrand sse sse2 sse4_1 sse4_2 ssse3" ;;
 		*': Intel(R) Xeon(R) CPU E3-'*' v5 @ '*)
 			use_cpu_arch='x86'
 			use_cpu_flags="aes avx avx2 f16c fma3 mmx mmxext pclmul popcnt rdrand sse sse2 sse3 sse4_1 sse4_2 ssse3" ;;
@@ -74,8 +92,56 @@ else
 			use_cpu_arch='arm'
 			use_cpu_flags="edsp neon thumb vfp vfpv3 vfpv4 vfp-d32 crc32 v4 v5 v6 v7 thumb2" ;;
 
+		*': 0xd07'|'Apple M1')
+			use_cpu_arch='arm'
+			use_cpu_flags="aes crc32 sha1 sha2" ;;
 		*)
-			echo >&2 "Unknown CPU '$( echo "${description}" | cut -d':' -f 2- | sed 's/^\s*// ; s/\s*$//' )' and 'cpuid2cpuflags' not installed - not enabling model-specific CPU flags" ;;
+			description="$( echo "${description}" | cut -d':' -f 2- | sed 's/^\s*// ; s/\s*$//' )"
+			vendor="$( grep -- '^vendor_id' /proc/cpuinfo | tail -n 1 | awk -F': ' '{ print $2 }' )"
+			if [ -r /proc/cpuinfo ]; then
+				case "${vendor}" in
+					GenuineIntel)
+						#echo >&2 "Attempting to auto-discover CPU '${description}' capabilities..."
+						use_cpu_arch='x86'
+
+						if
+							[ -f /var/db/repo/gentoo/profiles/desc/cpu_flags_x86.desc ] &&
+							[ -s /var/db/repo/gentoo/profiles/desc/cpu_flags_x86.desc ]
+						then
+							use_cpu_flags=''
+
+							# FIXME: Hard-coded /var/db/repo/gentoo...
+							while read -r line; do
+								echo "${line}" | grep -q -- ' - ' || continue
+
+								flag="$( echo "${line}" | awk -F' - ' '{ print $1 }' )"
+								if echo "${line}" | grep "^${flag} - " | grep -Fq -- '[' ; then
+									count=2
+									while true; do
+										extra="$( echo "${line}" | awk -F'[' "{ print \$${count} }" | cut -d']' -f 1 )"
+										if [ -n "${extra:-}" ]; then
+											flag="${flag}|${extra}"
+										else
+											break
+										fi
+										: $(( count = count + 1 ))
+									done
+									unset extra count
+								fi
+								use_cpu_flags="${use_cpu_flags:-}$( grep -E -- '^(Features|flags)' /proc/cpuinfo | tail -n 1 | awk -F': ' '{ print $2 }' | grep -Eq -- "${flag}" && echo " ${flag}" | cut -d'|' -f 1 )"
+							done < /var/db/repo/gentoo/profiles/desc/cpu_flags_x86.desc
+
+							use_cpu_flags="${use_cpu_flags# }"
+							echo >&2 "Auto-discovered CPU feature-flags '${use_cpu_flags}'"
+						fi
+						;;
+				esac
+			fi
+
+			if [ -z "${use_cpu_flags:-}" ]; then
+				echo >&2 "Unknown CPU '${description}' and 'cpuid2cpuflags' not installed - not enabling model-specific CPU flags"
+			fi
+			;;
 	esac
 fi
 if [ -n "${use_cpu_flags:-}" ]; then
@@ -108,6 +174,16 @@ fi
 #
 use_essential="asm ipv6 ithreads mdev nptl threads tls-heartbeat zlib${use_cpu_flags:+ ${use_cpu_flags}}"
 
+# Even though we often want a minimal set of flags, gcc's flags are significant
+# since they may affect the compiler facilities available to all packages built
+# later...
+#
+# N.B. USE='graphite' pulls-in dev-libs/isl which we don't want for host
+#      packages, but is reasonable for build-containers.
+#
+# FIXME: Source these flags from package.use
+use_essential_gcc="-fortran graphite nptl openmp pch sanitize ssp vtv zstd"
+
 case "$( uname -m )" in
 	x86_64|i686)
 		# Enable pypy support for Portage accleration of ~35%!
@@ -119,6 +195,7 @@ case "$( uname -m )" in
 			use_pypy="${use_pypy} dev-python/pypy3-exe-bin"
 			use_pypy_use="${use_pypy_use} low-memory"
 		fi
+		use_pypy_post_remove="dev-lang/python:2.7"
 		;;
 esac
 
@@ -128,13 +205,22 @@ bold="$( printf '\e[1m' )"
 red="$( printf '\e[31m' )"
 green="$( printf '\e[32m' )"
 blue="$( printf '\e[34m' )"
+purple="$( printf '\e[35m' )"
 # Place 'reset' last to prevent coloured xtrace output!
 reset="$( printf '\e[0m' )"
 
 # Export portage job-control variables...
 #
-jobs="$( echo "$( nproc ) 0.75 * p" | dc | cut -d'.' -f 1 )"
-: $(( load = $( nproc ) - 1 ))
+: $(( jobs = $( nproc ) ))
+: $(( load = jobs ))
+if [ $(( jobs )) -ge 2 ]; then
+	if command -v dc >/dev/null 2>&1; then
+		jobs="$( echo "${jobs} 0.75 * p" | dc | cut -d'.' -f 1 )"
+	else
+		: $(( jobs = jobs - 1 ))
+	fi
+	: $(( load = load - 1 ))
+fi
 export JOBS="${jobs}"
 export MAXLOAD="${load}.00"
 unset load jobs
@@ -178,4 +264,4 @@ if [ -f common/local.sh ]; then
 	export JOBS MAXJOBS TMPDIR
 fi
 
-# vi: set nowrap sw=4 ts=4:
+# vi: set colorcolumn=80 nowrap sw=4 ts=4:
