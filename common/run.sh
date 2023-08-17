cb3036c126a5b0b4111763419d51dff50b23a5dc
Stuart Shelton <stuart@shelton.me>
2023-08-17 23:40:33 +0100

    Sync common tools, licences, and USE flags
    Signed-off-by: Stuart Shelton <stuart@shelton.me>

--- common/run.sh
+++ common/run.sh
@@ -110,7 +110,7 @@ print() {
 		if [ -z "${*:-}" ]; then
 			output >&2
 		else
-			output >&2 "DEBUG: ${BASH_SOURCE[0]:-"$( basename "${0}" )"}: ${*}"
+			output >&2 "DEBUG: ${BASH_SOURCE[-1]:-"$( basename "${0}" )"}/${BASH_SOURCE[0]}: ${*}"
 		fi
 		return 0
 	# Unhelpful with 'set -e' ...
@@ -680,7 +680,8 @@ _docker_run() {
 			"${build_name#*/}:latest")
 				ext='.build' ;;
 			*)
-				die "I don't know how to apply DEV_MODE to image '${image}'"
+				ext='.build'
+				warn "I don't know how to apply DEV_MODE to image '${image}' - guessing 'entrypoint.sh${ext}'"
 				;;
 		esac
 		unset name
@@ -967,11 +968,14 @@ _docker_run() {
 			local arg=''
 			print "Starting ${BUILD_CONTAINER:+build} container with command '${_command} container run \\"
 			for arg in "${runargs[@]}"; do
-				print "        ${arg} \\"
+				case "${arg}" in
+					--*)	print "    ${arg} \\" ;;
+					*)		print "        ${arg} \\" ;;
+				esac
 			done
-			print "    ${image}"
-			for arg in "${@}"; do
-				print "        ${arg} \\"
+			print "  ${image}${@:+" \\"}"
+			for arg in "${@:-}"; do
+				[[ -n "${arg:-}" ]] && print "    ${arg} \\"
 			done
 			print "'"
 			unset arg
@@ -1115,7 +1119,9 @@ if ! type -pf podman >/dev/null 2>&1; then
 	docker_readonly='readonly'
 else
 	_command='podman'
-	docker() { podman --db-backend sqlite ${@+"${@}"} ; }
+	docker() {
+		podman ${@+"${@}"}
+	}  # docker
 
 	#extra_build_args='--format docker'
 	# From release 2.0.0, podman should accept docker 'readonly' attributes
